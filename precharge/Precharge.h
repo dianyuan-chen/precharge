9c6e278fc96762c15c352982c648938decc551b9
chendianyuan <dianyuan.chen@udantech.com>
2021-09-17 18:17:02 +0800

    预充第二版修改

--- precharge/Precharge.h
+++ precharge/Precharge.h
@@ -10,8 +10,8 @@ clock_t start, end;
 
 void PrechargeM_Start();	//启动预充
 void PrechargeM_Stop();	//停止预充
-void ChargeM_Start();	//启动预充
-void ChargeM_Stop();	//停止预充
+void ChargeM_Start();	//启动总正
+void ChargeM_Stop();	//停止总正
 BOOL PrechargeM_IsFinish();	//判断V1是否大于95%Bat
 BOOL PrechargeM_IsFail();	//超时
 
